dd5dd1b8f6468c24b3067383646367ab19ae116e
dian <dianjeruto@gmail.com>
2023-10-24 20:51:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+var upperCaseCustomerName=function(){
+  customerName=customerName.toUpperCase();
+}
+
+var setBestCustomer=function(){
+  bestCustomer ="not bob";
+}
+
+var overwriteBestCustomer =function(newBestCustomer){
+  bestCustomer =newBestCustomer
+}
+
+const leastFavoriteCustomer="lavin";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer ="leastFavoriteCustomer ";
+}
+
